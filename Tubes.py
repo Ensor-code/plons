89f7713c8ac4d88e2379ff52bcd2c889e062a687
Silke Maes <silke.maes@kuleuven.be>
2020-12-02 17:46:52 +0100

    Add files via upload

--- Tubes.py
+++ Tubes.py
@@ -170,22 +170,8 @@ Get the tube data and plots:
 '''
 def main_tube(run, outloc, setup, data):
     
-    print('')
-    print('(7)  Start calculations for the radial structure plots, using tubes.')
-    
-        
-    ### --- fig using the orb plane 'tube'
-    z_tube, orb_tube = getTubeData_orbPlane(setup,data)
-    
-    fig1 = plt.figure(figsize=(4,3.5))
-    ax1  = plt.subplot(111)
-
-    ax1.plot(orb_tube[0], orb_tube[1] ,c='k', ls = '--'  , lw = 0.8, label = 'orbital plane')
-    ax1.plot(z_tube[0]  , z_tube[1]   ,c='c',              lw = 0.8, label = 'polar axis')
-    
-        
     Mdot  = setup['Mdot' ]
-    #Select ylimits as in 1Dplots
+    # Select ylimits as in 1Dplots
     #if   Mdot <= 5e-7:
         #rhoMin = 10**(-21)  
         #rhoMax = 10**(-14)  
@@ -197,12 +183,30 @@ def main_tube(run, outloc, setup, data):
     #elif 5e-7 < Mdot < 1e-5:
         #rhoMin = 10**(-20)  
         #rhoMax = 10**(-13)  
+    
+    print('')
+    print('(7)  Start calculations for the radial structure plots, using tubes.')
+    
+        
+    ### --- fig using the orb plane 'tube'
+    z_tube, orb_tube = getTubeData_orbPlane(setup,data)
+    
+    fig1 = plt.figure(figsize=(4,3))
+    ax1  = plt.subplot(111)
+
+    ax1.plot(orb_tube[0], orb_tube[1] ,c='k', ls = '--'  , lw = 0.8, label = 'orbital plane')
+    ax1.plot(z_tube[0]  , z_tube[1]   ,c='c',              lw = 0.8, label = 'polar axis')
 
     ax1.set_yscale('log')
-    ax1.set_xlabel('$r$ [AU]', fontsize = 9)
-    ax1.set_ylabel('mean density [g/cm$^3$]', fontsize = 9)
+    ax1.set_xlabel('$r$ [AU]', fontsize = 8)
+    ax1.set_xlim([orb_tube[0][0],orb_tube[0][-1]])
+    ax1.set_ylabel('mean density [g/cm$^3$]', fontsize = 8)
     ax1.tick_params(labelsize=7)
-    ax1.set_ylim(rhoMin,rhoMax)
+    #ax1.set_ylim(rhoMin,rhoMax)
+    
+    ax_extra = ax1.twinx()
+    ax_extra.plot(z_tube[0],  (np.log(orb_tube[1]) - np.log(z_tube[1])) ,c='r', ls = '-'  , lw = 0.8, label = 'niet log')
+
 
     ax1.legend(fontsize = 7, loc = 'upper right')
     fig1.tight_layout()
@@ -210,20 +214,49 @@ def main_tube(run, outloc, setup, data):
     plt.savefig(outloc+str(run)+'_1D_tube_orb.png', dpi = 200)
     
     
-    #print(len(orb_tube[1]), len(z_tube[1]))    
-    fig2 = plt.figure(figsize=(4,3.5))
-    ax2  = plt.subplot(111)
-    ax2.plot(z_tube[0],  (np.log(orb_tube[1]) - np.log(z_tube[1])) ,c='r', ls = '-'  , lw = 0.8, label = 'niet log')
+    ### --- fig using the orb plane 'tube' & plotting the difference
+    diff = []
+    r = []
 
+    for i in range(len(orb_tube[0])):
+        for j in range(len(z_tube[0])):
+            if orb_tube[0][i] == z_tube[0][j]:
+                r.append(orb_tube[0][i])
+                diff.append(np.log10(orb_tube[1][i]) - np.log10(z_tube[1][j]))   
+    diff= tl.smoothen(diff,2)
+ 
+    
+    ax_extra = ax1.twinx()
+    ax_extra.plot(r,  diff            ,c='grey'        , lw = 0.3, label = 'difference' )
+    ax_extra.plot([r[0],r[-1]], [0,0] ,c='grey', ls=':', lw = 0.4)
+    color = 'tab:grey'
+    ax_extra.set_ylabel('difference', fontsize = 9, color=color)
+    ax_extra.set_ylim([-0.6,2.1])
+    ax_extra.tick_params(axis='y',labelsize=7)
 
-    ax2.set_xlabel('$r$ [AU]', fontsize = 9)
-    ax2.set_title('Mean density orb plane - mean density polar axis [g/cm$^3$]', fontsize = 9)
-    ax2.tick_params(labelsize=7)
+    ax1.legend(fontsize = 6, loc = 'upper center')
+    # ax_extra.legend(fontsize = 5, loc = 'upper right')
+    fig1.tight_layout()
 
-    fig2.tight_layout()
+    plt.savefig(outloc+str(run)+'_1D_tube_orb_diff.png', dpi = 200)
+    
+    
+    ### --- fig using the orb plane 'tube' ONLY plotting the difference
+    fig3 = plt.figure(figsize=(4,3.5))
+    ax3  = plt.subplot(111)
+    ax3.plot(z_tube[0],  (np.log(orb_tube[1]) - np.log(z_tube[1])) ,c='r', ls = '-'  , lw = 0.8, label = 'niet log')
+
+    ax3.set_xlabel('$r$ [AU]', fontsize = 9)
+    ax3.set_title('Mean density orb plane - mean density polar axis [g/cm$^3$]', fontsize = 9)
+    ax3.tick_params(labelsize=7)
+
+    fig3.tight_layout()
     
     plt.savefig(outloc+str(run)+'_1D_tube_difference.png', dpi = 200)
     
+    
+    
+    ### --- put data in a text file    
     title = outloc+str(run)+'_data_1D_tube.txt'
     with open (title,'w') as f:
         f.write('Model '+str(run)+'\n')
